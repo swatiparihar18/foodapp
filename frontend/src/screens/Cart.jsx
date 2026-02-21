e46be906fa80ca5e04a8cf51a4f12a3fb0b82255
Swati Parihar <swatiparihar577@gmail.com>
2026-02-21 13:26:38 +0530

    Update API endpoint URL in Cart.jsx

--- frontend/src/screens/Cart.jsx
+++ frontend/src/screens/Cart.jsx
@@ -42,7 +42,7 @@ const Cart = () => {
       }));
 
       // Use the globalFetch function
-      const result = await globalFetch('http://localhost:5000/api/orderData', {
+      const result = await globalFetch('http://54.226.28.119:5000/api/orderData', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
